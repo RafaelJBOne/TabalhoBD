f1999d235732cdfade88927d5f68c21f515e4297
Andre-iglesias <55508949+Andre-iglesias@users.noreply.github.com>
2022-09-19 15:11:49 -0300

    inserts

---
+++ clube.sql
@@ -0,0 +1,150 @@
+
+
+insert into Pais
+values
+(1,'Brasil'),
+(2,'Vietna'),
+(3,'Mexico'),
+(4,'Chile'),
+(5,'Belgica');
+
+insert into UF
+values
+(1,'Piaui',1),
+(2,'Dong',2),
+(3,'Caracas',3),
+(4,'Andes',4),
+(5,'Sla',5);
+
+insert into Cidade
+values
+(1,55,'Rua de tal',1),
+(2,59,'Rua de Caracas',2),
+(3,95,'Rutal',3),
+(4,84,'Rude',4),
+(5,66,'Rual',5);
+
+insert into Bairro 
+values
+(1,'St Oficina',1),
+(2,'St Escolar',2),
+(3,'St Militar',3),
+(4,'St Medico',4),
+(5,'St Bancario',5);
+
+
+insert into Tipo_Telefone
+values
+(1,'Residencial'),
+(2,'Comercial');
+
+insert into Tipo_Pagamento
+values
+(1,'Cheque'),
+(2,'Credito'),
+(3,'Debito');
+
+insert into Tipo_Endereço
+values
+(1,'Residencial'),
+(2,'Comercial');
+
+insert into Tipo_Cartão
+values
+(1,'Funcional'),
+(2,'Departamento'),
+(3,'Pessoal'),
+(4,'Recarregavel'),
+(5,'Comercial');
+
+insert Cargo
+values
+(1,'Pesquisador'),
+(2,'Desenvolvedor'),
+(3,'Alfaiate'),
+(4,'Trader'),
+(5,'Caberubero');
+
+
+insert Estado_civil
+values
+(1,'Solteiro'),
+(2,'Casado'),
+(3,'Viuvo');
+
+
+insert into Genero
+values
+(1,'Cis, Masculino'),
+(2,'Cis, Feminino'),
+(3,'tran, Masculino'),
+(4,'trans, Feminino');
+
+insert into Filiação
+values
+(1,'Jose Aldo','Gessica Marreta'),
+(2,'Zeca Pagodinho','Karol com K'),
+(3,'Bruno&Marrone','Ines Brasil'),
+(4,'Joao Gomes','Maisa Bom dia e CIA'),
+(5,'Biruliro','Damares');
+
+
+insert into Escolaridade
+values
+(1,'Superior'),
+(2,'Tecnico'),
+(3,'Medio'),
+(4,'Fundamental');
+
+insert into pagamento
+values
+(1,'200 RS',3),
+(2,'100 RS',2),
+(3,'80 RS',1),
+(4,'20 RS',3);
+
+insert into Produto
+values
+(1,'Arroz Carreteiro',null,'Arroz feito na carreta',1),
+(2,'Feijão Tropeiro',null,'Feijão feito na tropa',1),
+(3,'Muqueca',null,'Peixe com leite de coco',2),
+(4,'Cerveja',null,'A cerveja que tiver',3),
+(5,'Coca-Cola',null,'Sugiro água',4);
+
+insert into telefone
+values
+(1,'1234-5678',1,1),
+(2,'1111-1111',1,2),
+(3,'3652-9822',2,3),
+(4,'1548-6529',2,4),
+(5,'2564-6325',2,5);
+
+insert into endereço
+values
+(1,001,'12345-010',null,null,1,1),
+(2,111,'45369-000',null,null,1,2),
+(3,203,'62591-233',null,null,2,3),
+(4,605,'36503-070',null,null,2,4),
+(5,301,'69850-010',null,null,1,5);
+
+insert into pessoa
+values
+(1,'Gabriel Melo','652.658.986-35','2000-12-03',1,1,1,1,1,1,1,1,1),
+(2,'Lucca Cardoso','265.987.332-95','1966-11-06',1,2,2,2,2,2,2,2,2),
+(3,'Evelyn Melo','225.146.998-36','1979-06-24',2,3,3,3,3,1,1,3,3),
+(4,'Sophia','654.269.375-99','1989-01-02',4,4,4,4,4,4,2,4,4),
+(5,'Ana','221.545.344-62','1993-09-26',2,5,5,5,5,4,1,2,5),   
+(6,'Miguel','364.656.234-22','1958-12-31',3,3,2,2,2,2,2, 2,2);
+
+insert into Sócio
+values
+(1,null,'S.Senior',1),
+(2,null,'S.Pleno',2),
+(3,null,'S.Premium',3),
+(4,null,'S.Senior',4),
+(5,null,'S.Suspect',5);
+
+insert into Dependente
+values
+(1,null,'Irmao',6);
+
